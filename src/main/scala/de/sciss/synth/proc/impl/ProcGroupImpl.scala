4016c2c7d96c06eaf59049c561f47b7ced151e61
Sciss <contact@sciss.de>
2012-03-28 15:30:13 +0100

    step

--- src/main/scala/de/sciss/synth/proc/impl/ProcGroupImpl.scala
+++ src/main/scala/de/sciss/synth/proc/impl/ProcGroupImpl.scala
@@ -27,15 +27,34 @@ package de.sciss.synth.proc
 package impl
 
 import de.sciss.lucre.stm.Sys
-import de.sciss.lucre.DataOutput
 import de.sciss.collection.txn.{HASkipList, SkipList, Ordering => TxnOrdering}
+import de.sciss.lucre.{DataInput, event => evt, DataOutput}
+import evt.Compound
 
 object ProcGroupImpl {
    private val SER_VERSION = 0
 
    def empty[ S <: Sys[ S ]]( implicit tx: S#Tx ) : ProcGroup[ S ] = new New[ S ]( tx )
 
-   private sealed trait Impl[ S <: Sys[ S ]] extends ProcGroup[ S ] {
+   @volatile private var declMap = Map.empty[ Class[ _ ], Decl[ _ ]]
+
+   private def getDecl[ S <: Sys[ S ]]( implicit tx: S#Tx ) : Decl[ S ] = {
+      val clz = tx.system.manifest.erasure
+      declMap.getOrElse( clz, {
+         val declNew = new Decl[ S ]
+         declMap += clz -> declNew
+         declNew
+      }).asInstanceOf[ Decl[ S ]]
+   }
+
+   private class Decl[ S <: Sys[ S ]] extends evt.Decl[ S, Impl[ S ]] {
+      val serializer: evt.Reader[ S, Impl[ S ]] = new evt.Reader[ S, Impl[ S ]] {
+         def read( in: DataInput, access: S#Acc, targets: evt.Targets[ S ])( implicit tx: S#Tx ) : Impl[ S ] =
+            sys.error( "TODO" )
+      }
+   }
+
+   private sealed trait Impl[ S <: Sys[ S ]] extends ProcGroup[ S ] with Compound[ S, Impl[ S ], Decl[ S ]] {
       protected def seq: SkipList[ S, Proc[ S ]]
 
       final def add( procs: Proc[ S ]* )( implicit tx: S#Tx ) {
@@ -48,15 +67,13 @@ object ProcGroupImpl {
          sys.error( "TODO" )
       }
 
-      final def write( out: DataOutput ) {
+      final protected def writeData( out: DataOutput ) {
          out.writeUnsignedByte( SER_VERSION )
-         id.write( out )
          seq.write( out )
          sys.error( "TODO" )
       }
 
-      final def dispose()( implicit tx: S#Tx ) {
-         id.dispose()
+      final protected def disposeData()( implicit tx: S#Tx ) {
          seq.dispose()
          sys.error( "TODO" )
       }
@@ -69,7 +86,9 @@ object ProcGroupImpl {
       }
 
    private final class New[ S <: Sys[ S ]]( tx0: S#Tx ) extends Impl[ S ] {
-      val id            = tx0.newID()
+      protected val decl      = getDecl[ S ]( tx0 )
+      protected val targets   = evt.Targets[ S ]( tx0 )
+
       protected val seq = {
          implicit val tx      = tx0
          implicit val procOrd = procOrdering[ S ]
