7d88e048afaa140120d8ee008177bd955ba6f871
Sciss <contact@sciss.de>
2012-03-28 13:40:08 +0100

    try with graph serialization. ça marche!

--- src/test/scala/de/sciss/synth/proc/FirstTest.scala
+++ src/test/scala/de/sciss/synth/proc/FirstTest.scala
@@ -1,7 +1,8 @@
 package de.sciss.synth.proc
 
 import de.sciss.lucre.stm.InMemory
-import de.sciss.synth.expr
+import de.sciss.synth
+import synth.{SynthDef, Server, expr}
 import expr._
 
 // for f***'s sake, `extends App` doesn't work, no method `main` found ???
@@ -13,13 +14,29 @@ object FirstTest {
       implicit val whyOhWhy = Proc.serializer[ S ]
 
       val cursor: S = InMemory()
-      val access = cursor.root { implicit tx => Proc[S]() }
+      val access = cursor.root { implicit tx => Proc[ S ]() }
 
       cursor.step { implicit tx =>
          val p = access.get
          println( "Old name is " + p.name )
          p.name = "Schnuckendorfer"
          println( "New name is " + p.name )
+         p.graph = {
+            import synth._; import ugen._
+            val f = LFSaw.kr( 0.4 ).madd( 24, LFSaw.kr( Seq( 8, 7.23 )).madd( 3, 80 )).midicps
+            val c = CombN.ar( SinOsc.ar( f ) * 0.04, 0.2, 0.2, 4 )
+            Out.ar( 0, c )
+         }
+      }
+
+      val gr = cursor.step { implicit tx => access.get.graph }
+      println( "Graph now " + gr )
+
+      Server.run { s =>
+         val sd = SynthDef( "test", gr.expand )
+         sd.play
+         Thread.sleep( 4000 )
+         sys.exit( 0 )
       }
    }
 }
