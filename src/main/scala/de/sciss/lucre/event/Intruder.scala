bd8efc987418eb43478968c1c52663b063ce3da3
Sciss <contact@sciss.de>
2012-05-01 21:34:55 +0100

    tests run without errors. still should verify the results -- have the feeling there might be a lost freqVar propagation message

--- src/main/scala/de/sciss/lucre/event/Intruder.scala
+++ src/main/scala/de/sciss/lucre/event/Intruder.scala
@@ -1,13 +1,13 @@
-package de.sciss.lucre.event
-
-import de.sciss.lucre.stm.Sys
-
-object Intruder {
-   def --->[ S <: Sys[ S ]]( e: EventLike[ S, _, _ ], r: ExpandedSelector[ S ])( implicit tx: S#Tx ) {
-      e ---> r
-   }
-
-   def -/->[ S <: Sys[ S ]]( e: EventLike[ S, _, _ ], r: ExpandedSelector[ S ])( implicit tx: S#Tx ) {
-      e -/-> r
-   }
-}
+//package de.sciss.lucre.event
+//
+//import de.sciss.lucre.stm.Sys
+//
+//object Intruder {
+//   def --->[ S <: Sys[ S ]]( e: EventLike[ S, _, _ ], r: ExpandedSelector[ S ])( implicit tx: S#Tx ) {
+//      e ---> r
+//   }
+//
+//   def -/->[ S <: Sys[ S ]]( e: EventLike[ S, _, _ ], r: ExpandedSelector[ S ])( implicit tx: S#Tx ) {
+//      e -/-> r
+//   }
+//}
