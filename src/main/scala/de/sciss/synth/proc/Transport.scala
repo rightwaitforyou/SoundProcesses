7be1bdf7d107fd6ffcf79c60a5812c0a2f9812aa
Sciss <contact@sciss.de>
2012-05-07 20:49:44 +0100

    ....

--- src/main/scala/de/sciss/synth/proc/Transport.scala
+++ src/main/scala/de/sciss/synth/proc/Transport.scala
@@ -1,9 +1,20 @@
 package de.sciss.synth.proc
 
-import de.sciss.lucre.stm.Sys
-import de.sciss.lucre.expr.Chronos
+import de.sciss.lucre.expr.{BiType, Expr, Chronos}
+import de.sciss.lucre.stm.{Writer, Disposable, Sys}
 
-trait Transport[ S <: Sys[ S ]] {
-   def play()( implicit time: Chronos[ S ]) : Unit
-   def stop()( implicit time: Chronos[ S ]) : Unit
+object Transport {
+   def apply[ S <: Sys[ S ]]( group: ProcGroup[ S ], sampleRate: Double = 44100 )
+                            ( implicit tx: S#Tx /*, longs: BiType[ Long ]*/) : Transport[ S ] =
+      impl.TransportImpl( group, sampleRate )
+}
+trait Transport[ S <: Sys[ S ]] extends Chronos[ S ] with Writer with Disposable[ S#Tx ] {
+   def seek( time: Long )( implicit tx: S#Tx ) : Unit
+   def playing( implicit tx: S#Tx ) : Expr[ S, Boolean ]
+   def playing_=( expr: Expr[ S, Boolean ])( implicit tx: S#Tx ) : Unit
+
+   def sampleRate: Double
+
+//   def play()( implicit time: Chronos[ S ]) : Unit
+//   def stop()( implicit time: Chronos[ S ]) : Unit
 }
---
+++ src/main/scala/de/sciss/synth/proc/impl/TransportImpl.scala
@@ -0,0 +1,57 @@
+package de.sciss.synth.proc
+package impl
+
+import de.sciss.lucre.stm.Sys
+import de.sciss.lucre.expr.{Expr, BiType}
+import de.sciss.lucre.DataOutput
+import de.sciss.lucre.event.{Reader, Targets, Change, Root}
+import de.sciss.synth.expr.{Longs, Booleans}
+
+object TransportImpl {
+   def apply[ S <: Sys[ S ]]( group: ProcGroup[ S ], sampleRate: Double )
+                            ( implicit tx: S#Tx /*, longs: BiType[ Long ]*/) : Transport[ S ] =
+      new Impl[ S ]( group, sampleRate, tx )
+
+   private final class TimeExpr[ S <: Sys[ S ]]( protected val targets: Targets[ S ], t: Impl[ S ])
+   extends Expr.Node[ S, Long ]
+   with Root[ S, Change[ Long ]] {
+      protected def writeData( out: DataOutput ) {
+         out.writeUnsignedByte( 4 )
+         t.write( out )
+      }
+
+      def value( implicit tx: S#Tx ) : Long = sys.error( "TODO" )
+
+      protected def reader : Reader[ S, Expr[ S, Long ]] = Longs.serializer[ S ]
+   }
+
+   private final class Impl[ S <: Sys[ S ]]( group: ProcGroup[ S ],
+                                             val sampleRate: Double, tx0: S#Tx )
+   extends Transport[ S ] {
+      override def toString = "Transport(" + sampleRate + ")"
+
+      private val timeExpr    = new TimeExpr( Targets[ S ](tx0 ), this )
+      private val playingVar  = Booleans.newVar[ S ]( Booleans.newConst( false ))( tx0 )
+      private val systemRef   = System.currentTimeMillis()
+
+      def write( out: DataOutput ) {
+         playingVar.write( out )
+      }
+
+      def dispose()( implicit tx: S#Tx ) {
+         playingVar.dispose()
+      }
+
+      def seek( time: Long )( implicit tx: S#Tx ) {
+         sys.error( "TODO" )
+      }
+
+      def playing( implicit tx: S#Tx ) : Expr[ S, Boolean ] = playingVar.get
+
+      def playing_=( expr: Expr[ S, Boolean ])( implicit tx: S#Tx ) {
+         playingVar.set( expr )
+      }
+
+      def time : Expr[ S, Long ] = timeExpr
+   }
+}
