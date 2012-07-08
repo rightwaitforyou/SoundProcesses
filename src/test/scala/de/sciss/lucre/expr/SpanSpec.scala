ba460ee3bb77de5b1cecf1bbe872d862aaff1971
Sciss <contact@sciss.de>
2012-07-08 01:00:07 +0100

    union test ok

--- src/test/scala/de/sciss/lucre/expr/SpanSpec.scala
+++ src/test/scala/de/sciss/lucre/expr/SpanSpec.scala
@@ -8,6 +8,9 @@ class SpanSpec extends FlatSpec with ShouldMatchers {
    val span1   = Span( 30, 40 )
    val span2   = Span( 40, 50 )
    val span3   = Span( 40, 40 )
+   val span4   = Span( 30, 50 )
+   val span5   = Span( 35, 45 )
+   val span6   = Span( 31, 39 )
    val sfrom   = Span.from( 40 )
    val suntl   = Span.until( 40 )
    val salle   = Span.all
@@ -15,13 +18,10 @@ class SpanSpec extends FlatSpec with ShouldMatchers {
    val sseq    = IIdxSeq[ SpanLike ]( span1, span2, span3, sfrom, suntl, salle, svoid )
    val spans   = IIdxSeq( span1, span2, span3 )
    val sseqi   = sseq.zipWithIndex
+   val sseq2   = sseq ++ IIdxSeq( span4, span5, span6 )
    val pos     = IIdxSeq( 25, 30, 35, 40, 45, 50, 55 )
    val shifts  = IIdxSeq( -3, 0, 3 )
 
-   val span4   = Span( 30, 50 )
-   val span5   = Span( 35, 45 )
-   val span6   = Span( 31, 39 )
-   val sseq2   = sseq ++ IIdxSeq( span4, span5, span6 )
    val mcont   = IIdxSeq(
    // span1  span2  span3  sfrom  suntl  salle  svoid  span4  span5  span6
       true,  false, true,  false, false, false, false, false, false, true,    // span1
@@ -61,6 +61,37 @@ class SpanSpec extends FlatSpec with ShouldMatchers {
       true,  true,  true,  true,  true,  true,  false, true,  true,  true,    // span5
       true,  false, false, false, true,  true,  false, true,  true,  true     // span6
    )
+
+   val from30 = Span.from(30)
+   val from35 = Span.from(35)
+   val from31 = Span.from(31)
+   val untl50 = Span.until(50)
+   val untl45 = Span.until(45)
+   val span7  = Span(30,45)
+   val span8  = Span(31,40)
+   val span9  = Span(35,50)
+   val span10 = Span(31,45)
+   val span11 = Span(31,50)
+   val munion = IIdxSeq(
+   // span1   span2   span3   sfrom   suntl   salle  svoid  span4   span5   span6
+      span1,  span4,  span1,  from30, suntl,  salle, span1, span4,  span7,  span1,     // span1
+      span4,  span2,  span2,  sfrom,  untl50, salle, span2, span4 , span9,  span11,    // span2
+      span1,  span2,  span3,  sfrom,  suntl,  salle, span3, span4,  span5,  span8,     // span3
+      from30, sfrom,  sfrom,  sfrom,  salle,  salle, sfrom, from30, from35, from31,    // sfrom
+      suntl,  untl50, suntl,  salle,  suntl,  salle, suntl, untl50, untl45, suntl,     // suntl
+      salle,  salle,  salle,  salle,  salle,  salle, salle, salle,  salle,  salle,     // salle
+      span1,  span2,  span3,  sfrom,  suntl,  salle, svoid, span4,  span5,  span6,     // svoid
+      span4,  span4,  span4,  from30, untl50, salle, span4, span4,  span4,  span4,     // span4
+      span7,  span9,  span5,  from35, untl45, salle, span5, span4,  span5,  span10,    // span5
+      span1,  span11, span8,  from31, suntl,  salle, span6, span4,  span10, span6      // span6
+   )
+
+   //   val span1   = Span( 30, 40 )
+   //   val span2   = Span( 40, 50 )
+   //   val span3   = Span( 40, 40 )
+   //   val span4   = Span( 30, 50 )
+   //   val span5   = Span( 35, 45 )
+   //   val span6   = Span( 31, 39 )
 
    "A SpanLike" should "be consistently test for equality" in {
       sseqi.foreach { case (s1, idx1) =>
@@ -176,15 +207,31 @@ class SpanSpec extends FlatSpec with ShouldMatchers {
          val row = idx / sseq2.size
          val col = idx % sseq2.size
          val res1 = sseq2( row ).touches( sseq2( col ))
-         val res2 = sseq2( col ).touches( sseq2( row ))
-         val res3 = sseq2( row ).overlaps( sseq2( col ))
+         val commut = sseq2( col ).touches( sseq2( row ))
+         val overlaps = sseq2( row ).overlaps( sseq2( col ))
          res1 should equal (res)
-         res1 should equal (res2)                  // commutative
-         if( res3 ) res1 should equal (true)       // overlap is stronger condition
+         res1 should equal (commut)                   // commutative
+         if( overlaps ) res1 should equal (true)      // overlap is stronger condition
       }
    }
 
-   //  ; union, intersect, subtract
+   it should "correctly answer `union` with respect to other spans" in {
+      munion.zipWithIndex.foreach { case (res, idx) =>
+         val row     = idx / sseq2.size
+         val col     = idx % sseq2.size
+         val res1    = sseq2( row ).union( sseq2( col ))
+         val commut  = sseq2( col ).union( sseq2( row ))
+//         val touches = sseq2( row ).touches( sseq2( col ))
+         val contains = sseq2( row ).contains( sseq2( col ))
+//println( "" + row + " - " + col )
+         res1 should equal (res)
+         res1 should equal (commut)                // commutative
+//         if( !touches ) res1 should equal (svoid)
+         if( contains ) res1 should equal (sseq2( row ))
+      }
+   }
+
+   // intersect, subtract
 
    "A Span.Open" should "produce a correct `invert`" in {
       sfrom.invert should equal (suntl)
