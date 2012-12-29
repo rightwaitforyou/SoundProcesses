c009f1ba0936e3dacfc273aa6da0385ea8d414f0
Sciss <contact@sciss.de>
2012-12-29 13:36:35 +0100

    with Durable impl

---
+++ src/main/scala/de/sciss/synth/proc/Durable.scala
@@ -0,0 +1,41 @@
+/*
+ *  Durable.scala
+ *  (SoundProcesses)
+ *
+ *  Copyright (c) 2010-2013 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either
+ *  version 2, june 1991 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License (gpl.txt) along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.synth.proc
+
+import de.sciss.lucre.{stm, event => evt}
+import impl.{DurableImpl => Impl}
+import stm.{DataStoreFactory, DataStore}
+
+object Durable {
+   private type S = Durable
+
+   def apply( factory: DataStoreFactory[ DataStore ], mainName: String = "data", eventName: String = "event" ) : S =
+      Impl( factory, mainName, eventName )
+}
+trait Durable extends evt.DurableLike[ Durable ] with Sys[ Durable ]{
+   final type Tx  = Sys.Txn[ Durable ] with evt.DurableLike.Txn[ Durable ]
+   final type I   = evt.InMemory
+}
---
+++ src/main/scala/de/sciss/synth/proc/impl/DurableImpl.scala
@@ -0,0 +1,55 @@
+/*
+ *  DurableImpl.scala
+ *  (SoundProcesses)
+ *
+ *  Copyright (c) 2010-2013 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either
+ *  version 2, june 1991 of the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License (gpl.txt) along with this software; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.synth.proc
+package impl
+
+import concurrent.stm.InTxn
+import de.sciss.lucre.{event => evt, stm}
+import stm.{DataStoreFactory, DataStore}
+
+object DurableImpl {
+   def apply( factory: DataStoreFactory[ DataStore ], mainName: String, eventName: String ) : Durable = {
+      val mainStore  = factory.open( mainName )
+      val eventStore = factory.open( eventName )
+      new DurableSystem( mainStore, eventStore )
+   }
+
+   private final class TxnImpl( val system: DurableSystem, val peer: InTxn )
+   extends stm.impl.DurableImpl.TxnMixin[ Durable ] with evt.impl.DurableImpl.DurableTxnMixin[ Durable ]
+   with ProcTxnFullImpl[ Durable ] {
+      lazy val inMemory: evt.InMemory#Tx = system.inMemory.wrap( peer )
+      override def toString = "proc.Durable#Tx@" + hashCode.toHexString
+   }
+
+   private final class DurableSystem( protected val store: DataStore, protected val eventStore: DataStore )
+   extends evt.impl.DurableImpl.DurableMixin[ Durable, evt.InMemory ] with Durable
+   with evt.impl.ReactionMapImpl.Mixin[ Durable ] {
+      private type S = Durable
+      val inMemory: evt.InMemory = evt.InMemory()
+      def wrap( peer: InTxn ) : S#Tx = new TxnImpl( this, peer )
+      override def toString = "proc.Durable@" + hashCode.toHexString
+   }
+}
