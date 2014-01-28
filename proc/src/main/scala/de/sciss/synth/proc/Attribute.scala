1d39ed2b76f08f43380ab2cc5d1bd71bd744ce77
Sciss <contact@sciss.de>
2014-01-28 11:28:49 +0100

    simplify header

--- proc/src/main/scala/de/sciss/synth/proc/Attribute.scala
+++ proc/src/main/scala/de/sciss/synth/proc/Attribute.scala
@@ -1,3 +1,16 @@
+/*
+ *  Attribute.scala
+ *  (SoundProcesses)
+ *
+ *  Copyright (c) 2010-2014 Hanns Holger Rutz. All rights reserved.
+ *
+ *	This software is published under the GNU General Public License v2+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
 package de.sciss
 package synth
 package proc
--- synth-expr/src/main/scala/de/sciss/lucre/synth/expr/VecLikeType.scala
+++ synth-expr/src/main/scala/de/sciss/lucre/synth/expr/VecLikeType.scala
@@ -1,9 +1,21 @@
+/*
+ *  VecLikeType.scala
+ *  (SoundProcesses)
+ *
+ *  Copyright (c) 2010-2014 Hanns Holger Rutz. All rights reserved.
+ *
+ *	This software is published under the GNU General Public License v2+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
 package de.sciss.lucre.synth.expr
 
-import scala.collection.immutable.{Seq => ISeq, IndexedSeq => Vec}
+import scala.collection.immutable.{IndexedSeq => Vec}
 import de.sciss.serial.{ImmutableSerializer, DataOutput, DataInput}
 import de.sciss.lucre.event.{Targets, Sys}
-import de.sciss.lucre.expr.{Type, Expr}
 import de.sciss.lucre.bitemp.BiType
 
 trait VecLikeType[A] extends BiTypeImpl[Vec[A]] {
@@ -29,3 +41,21 @@ trait VecLikeType[A] extends BiTypeImpl[Vec[A]] {
 //  def size: Expr[S, Int]
 //  def get(index: Expr[S, Int]): Expr[S, Option[A]] // OptionExpr[S, A]
 //}
+
+object IntVec extends VecLikeType[Int] {
+  final val element = Ints
+
+  final val typeID = 0x2000 | Ints.typeID
+}
+
+object LongVec extends VecLikeType[Long] {
+  final val element = Longs
+
+  final val typeID = 0x2000 | Longs.typeID
+}
+
+object DoubleVec extends VecLikeType[Double] {
+  final val element = Doubles
+
+  final val typeID = 0x2000 | Doubles.typeID
+}
