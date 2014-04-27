d3bba889325586bf59b78b371bb5bf67faf2568e
Hanns Holger Rutz <contact@sciss.de>
2014-04-27 18:44:24 +0200

    re-enable these type aliases

--- proc/src/main/scala/de/sciss/synth/proc/Folder.scala
+++ proc/src/main/scala/de/sciss/synth/proc/Folder.scala
@@ -19,6 +19,8 @@ import impl.{FolderElemImpl => Impl}
 import de.sciss.serial
 import de.sciss.synth.proc
 import de.sciss.serial.{Serializer, DataInput}
+import scala.collection.immutable.{IndexedSeq => Vec}
+import language.existentials
 
 object Folder {
   def apply[S <: Sys[S]](implicit tx: S#Tx): Folder[S] = expr.List.Modifiable[S, Obj[S], Obj.Update[S]]
@@ -28,11 +30,11 @@ object Folder {
 
   // private[Folder] type _Update[S <: Sys[S]] = expr.List.Update[S, _Element[S], _Element.Update[S]]
 
-  //  type Update[S <: Sys[S]] = Vec[Change[S]]
-  //  sealed trait Change[S <: Sys[S]] { def obj: Obj[S] }
-  //  final case class Added  [S <: Sys[S]](idx: Int, obj: Obj[S]) extends Change[S]
-  //  final case class Removed[S <: Sys[S]](idx: Int, obj: Obj[S]) extends Change[S]
-  //  final case class Element[S <: Sys[S]](obj: Obj[S], update: Obj.Update[S, Any]) extends Change[S]
+  type Changes[S <: Sys[S]] = Vec[Change[S]]
+  sealed trait Change[S <: Sys[S]] { def obj: Obj[S] }
+  final case class Added  [S <: Sys[S]](idx: Int, obj: Obj[S]) extends Change[S]
+  final case class Removed[S <: Sys[S]](idx: Int, obj: Obj[S]) extends Change[S]
+  final case class Element[S <: Sys[S]](obj: Obj[S], update: Obj.Update[S]) extends Change[S]
 
   implicit def serializer[S <: Sys[S]]: Serializer[S#Tx, S#Acc, Folder[S]] =
     anySer.asInstanceOf[Serializer[S#Tx, S#Acc, Folder[S]]]
