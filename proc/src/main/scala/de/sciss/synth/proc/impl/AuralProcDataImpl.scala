rightwaitforyou/SoundProcesses

commit d0bb9fe83bd8c3d7249e8a8ebd9fb114b67c4639

fix(?) scan event bug

proc/src/main/scala/de/sciss/synth/proc/impl/ScanImpl.scala
@@ -19,7 +19,6 @@ import de.sciss.lucre.{event => evt, data, expr}
 import evt.{impl => evti, Event, Sys}
 import annotation.switch
 import expr.List
-import proc.Scan
 import de.sciss.serial.{DataOutput, Serializer, DataInput}
 import collection.immutable.{IndexedSeq => Vec}
 import de.sciss.lucre.synth.InMemory
@@ -100,7 +99,8 @@ object ScanImpl {
     extends Scan[S]
     with evti.StandaloneLike[S, Scan.Update[S], Scan[S]]
     with evti.Generator[S, Scan.Update[S], Scan[S]] {
-    override def toString() = "Scan" + id
+
+    override def toString() = s"Scan$id"
 
     def sinks  (implicit tx: S#Tx): data.Iterator[S#Tx, Link[S]] = sinkList.iterator
     def sources(implicit tx: S#Tx): data.Iterator[S#Tx, Link[S]] = new data.Iterator[S#Tx, Link[S]] {
@@ -194,12 +194,9 @@ object ScanImpl {
     def disconnect()(implicit tx: S#Tx): Unit = graphemeSourceList.changed -/-> this
 
     def pullUpdate(pull: evt.Pull[S])(implicit tx: S#Tx): Option[Scan.Update[S]] = {
-      val u1 = if (pull.parents(this).isEmpty)
-        pull.resolve[Scan.Update[S]].changes // fold(Vec.empty[Scan.Change[S]])(_.changes)
-      else
-        Vec.empty
-
-      val u2 = pull(graphemeSourceList.changed).fold(u1) { ll =>
+      val gen = pull.isOrigin(this)
+      val u1 = if (!gen) Vec.empty else pull.resolve[Scan.Update[S]].changes
+      val u2 = if ( gen) Vec.empty else pull(graphemeSourceList.changed).fold(u1) { ll =>
         val gcs = ll.changes.collect {
           case List.Element(_, gc) => Scan.GraphemeChange(gc.grapheme, gc.changes)
         }

commit da53a2968c8cd8a00c6a54284c1ebf943da71a2d

d'oh - fix another scan-event bug introduced in d0bb9fe83bd8c3d7249e8a8ebd9fb114b67c4639

proc/src/main/scala/de/sciss/synth/proc/impl/AuralProcDataImpl.scala
@@ -123,14 +123,14 @@ object AuralProcDataImpl {
           case Obj.ElemChange(Proc.Update(_, pCh)) =>
             pCh.foreach {
               case Proc.GraphChange(Change(_, newGraph)) => newSynthGraph(newGraph)
-              case Proc.ScanAdded(key, scan) => scanAdded(key, scan)
-              case Proc.ScanRemoved(key, scan) => scanRemoved(key, scan)
-              case Proc.ScanChange(key, scan, sCh) => scanChange(key, scan, sCh)
+              case Proc.ScanAdded  (key, scan)      => scanAdded  (key, scan)
+              case Proc.ScanRemoved(key, scan)      => scanRemoved(key, scan)
+              case Proc.ScanChange (key, scan, sCh) => scanChange (key, scan, sCh)
             }
 
-          case Obj.AttrAdded(key, value) => attrAdded(key, value)
-          case Obj.AttrRemoved(key, value) => attrRemoved(key, value)
-          case Obj.AttrChange(key, value, aCh) => attrChange(key, value, aCh)
+          case Obj.AttrAdded  (key, value)          => attrAdded  (key, value)
+          case Obj.AttrRemoved(key, value)          => attrRemoved(key, value)
+          case Obj.AttrChange (key, value, aCh)     => attrChange (key, value, aCh)
         }
       }
 
@@ -174,7 +174,7 @@ object AuralProcDataImpl {
 
     private def scanAdded(key: String, scan: Scan[S])(implicit tx: S#Tx): Unit = {
       logA(s"ScanAdded  to   ${procCached()} ($key)")
-      testInScan(key, scan)
+      testInScan (key, scan)
       testOutScan(key, scan)
     }
 

proc/src/main/scala/de/sciss/synth/proc/impl/ScanImpl.scala
@@ -196,7 +196,7 @@ object ScanImpl {
     def pullUpdate(pull: evt.Pull[S])(implicit tx: S#Tx): Option[Scan.Update[S]] = {
       val gen = pull.isOrigin(this)
       val u1 = if (!gen) Vec.empty else pull.resolve[Scan.Update[S]].changes
-      val u2 = if ( gen) Vec.empty else pull(graphemeSourceList.changed).fold(u1) { ll =>
+      val u2 = if ( gen) u1        else pull(graphemeSourceList.changed).fold(u1) { ll =>
         val gcs = ll.changes.collect {
           case List.Element(_, gc) => Scan.GraphemeChange(gc.grapheme, gc.changes)
         }